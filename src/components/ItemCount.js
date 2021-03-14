3724e34ba03a07ff29339b13dfdc28cc3686ddf2
Santiago <santruster@gmail.com>
2021-03-14 14:55:44 -0300

    added style

--- src/components/ItemCount.js
+++ src/components/ItemCount.js
@@ -3,10 +3,10 @@ const ItemCount = ({stock, initial, onAdd})=> {
   const [title, setTitle] = useState('')
     return (
       <div >
-          <form>
+          <form class="form-group">
           Stock: {stock}
-          <input id="cantidad" onChange={event => setTitle(event.target.value)} type="number" placeholder={initial}></input>
-          <button onClick={(e) => {onAdd(e,title)}}>Agregar al carrito</button>
+          <input class="form-control" id="cantidad" onChange={event => setTitle(event.target.value)} type="number" placeholder={initial}></input>
+          <button class="btn btn-primary" onClick={(e) => {onAdd(e,title)}}>Agregar al carrito</button>
           </form>  
       </div>
     );
